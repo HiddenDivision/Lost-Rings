6299e7328e010daf63a2c7d9848f80b3199d1222
HiddenDivision <52402557+HiddenDivision@users.noreply.github.com>
2019-07-19 16:17:17 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const testpre = '-';
 
 bot.on('ready', () =>{
 	console.log('Hidden division is now hidden.');
-	bot.user.setActivity('to you.', { type: ('LISTENING')})
+	bot.user.setActivity('you.', { type: ('LISTENING')})
 })
 
 bot.on('guildMemberAdd', member =>{
